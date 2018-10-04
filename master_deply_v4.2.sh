509b7a51193804bcf174b3fdf9be85c3be00c95c
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2018-10-04 16:47:39 +0530

    Update master_deply_v4.2.sh

--- master_deply_v4.2.sh
+++ master_deply_v4.2.sh
@@ -534,7 +534,7 @@ configure_Lambda_template()
 	     o=$IFS
              IFS=$(echo -en "\n\b")
 	     envvars=$( cat $listname.json | jq  -c ' .app_var ')
-	     echo $envvars > /home/circleci/project/config/dev.json
+	     echo -e "$envvars" > /home/circleci/project/config/dev.json
             #yq r $listname.json  >$listname.yml
             #a=serverless.yml
             #b="$listname.json"

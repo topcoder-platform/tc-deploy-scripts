975449ec9b42d4d84ebb4f9524f4b62f6324f89c
Gunasekar-K <Gunasekar.Kesavalu@wipro.com>
2022-01-18 11:24:06 +0530

    Update master_deploy.sh

--- master_deploy.sh
+++ master_deploy.sh
@@ -137,7 +137,8 @@ ECS_push_ecr_image() {
         ECS_TAG=$CIRCLE_BUILD_NUM
     fi
 	log "Pushing Docker Image..."
-	eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+	#eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+	aws ecr get-login-password --region $AWS_REGION | docker -D login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$ECS_TAG
 	track_error $? "ECS ECR image push"
 	log "Docker Image published."
@@ -155,7 +156,8 @@ ECSCLI_push_ecr_image() {
         ECS_TAG=$CIRCLE_BUILD_NUM
     fi
 	log "Pushing Docker Image..."
-	eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+	#eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+	aws ecr get-login-password --region $AWS_REGION | docker -D login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECS_REPONAME:$ECS_TAG
 	track_error $? "ECS ECR image push"
 	log "Docker Image published."
@@ -890,7 +892,8 @@ if [ "$DEPLOYMENT_TYPE" == "ECS" ]
 then
     if [ "$DEPLOYCATEGORY" == "CLI" ]
     then
-        eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+        #eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+	aws ecr get-login-password --region $AWS_REGION | docker -D login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
         #Moving image to repository
         if [ -z $APP_IMAGE_NAME ];
         then

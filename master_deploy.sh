80eb75a704f19c38a12eee6179d2c3271c2eeb51
Gunasekar-K <Gunasekar.Kesavalu@wipro.com>
2022-06-29 15:56:18 +0530

    Update master_deploy.sh

--- master_deploy.sh
+++ master_deploy.sh
@@ -156,9 +156,9 @@ ECS_push_ecr_image() {
     fi 
 
 	log "Pushing Docker Image..."
-	aws ecr get-login --region $AWS_REGION --no-include-email
-	# eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
-	aws ecr get-login-password --region $AWS_REGION | docker -D login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+	# aws ecr get-login --region $AWS_REGION --no-include-email
+	eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+	# aws ecr get-login-password --region $AWS_REGION | docker -D login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$ECS_TAG
 	track_error $? "ECS ECR image push"
 	log "Docker Image published."
@@ -176,9 +176,9 @@ ECSCLI_push_ecr_image() {
         ECS_TAG=tc-jb-$CIRCLE_BUILD_NUM
     fi
 	log "Pushing Docker Image..."
-	aws ecr get-login --region $AWS_REGION --no-include-email
-	# eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
-	aws ecr get-login-password --region $AWS_REGION | docker -D login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+	# aws ecr get-login --region $AWS_REGION --no-include-email
+	eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+	# aws ecr get-login-password --region $AWS_REGION | docker -D login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECS_REPONAME:$ECS_TAG
 	track_error $? "ECS ECR image push"
 	log "Docker Image published."

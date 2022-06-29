59d898706ec25791ce8a3fa6186664d4112a760d
Gunasekar-K <Gunasekar.Kesavalu@wipro.com>
2022-06-29 15:38:32 +0530

    Update master_deploy.sh

--- master_deploy.sh
+++ master_deploy.sh
@@ -156,6 +156,7 @@ ECS_push_ecr_image() {
     fi 
 
 	log "Pushing Docker Image..."
+	aws ecr get-login --region $AWS_REGION --no-include-email
 	# eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
 	aws ecr get-login-password --region $AWS_REGION | docker -D login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$ECS_TAG
@@ -175,6 +176,7 @@ ECSCLI_push_ecr_image() {
         ECS_TAG=tc-jb-$CIRCLE_BUILD_NUM
     fi
 	log "Pushing Docker Image..."
+	aws ecr get-login --region $AWS_REGION --no-include-email
 	# eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
 	aws ecr get-login-password --region $AWS_REGION | docker -D login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECS_REPONAME:$ECS_TAG
